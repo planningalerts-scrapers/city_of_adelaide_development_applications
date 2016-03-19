2e01b5ac6178691b989464239795abce39a2d86f
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-03-19 16:21:51 +1030

    More precisely target the exact inputField for where there are multiples

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,7 @@ search_result_url = 'https://ecouncil.burwood.nsw.gov.au/eservice/daEnquiryDetai
 def scrape_table(agent, scrape_url, comment_url)
   puts "Scraping " + scrape_url
   doc = agent.get(scrape_url)
-  rows = doc.search('.inputField').map { |e| e.inner_text.strip }
+  rows = doc.search('.rowDataOnly > .inputField:nth-child(2)').map { |e| e.inner_text.strip }
   reference = rows[2]
   date_received = Date.strptime(rows[3], '%d/%m/%Y').to_s rescue nil
   puts "Invalid date: #{rows[3].inspect}" unless date_received
