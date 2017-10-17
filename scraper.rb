5d78c1dcd7fc42e3c7d192d545e86dee3f7d738a
Eric Tam <eric.tam@traceyanderic.com>
2017-10-17 13:54:49 +1100

    added ability to scrape other types of applications like 'DA', 'S49', 'S10', 'HIS' and 'LD', fixes #1

--- scraper.rb
+++ scraper.rb
@@ -19,6 +19,7 @@ cookie_url = "https://epathway.adelaidecitycouncil.com/ePathway/ePathwayProd/Web
 search_url = "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd/web/GeneralEnquiry/externalrequestbroker.aspx?Module=EGELAP&Class=DEVT&Type=DEVT"
 base_url   = "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd/web/GeneralEnquiry/"
 comment_url = "mailto:city@cityofadelaide.com.au"
+daTypes = ['DA', 'S49', 'S10', 'HIS', 'LD']
 
 agent = Mechanize.new
 agent.user_agent_alias = 'Mac Safari'
@@ -43,51 +44,54 @@ def dbLookup(type = 'DA')
   maxApplication = i
 end
 
-type = 'DA'
-maxApplication = dbLookup(type)
-error = 0
-cont = true
-while cont do
-  form = page.form
-  form.field_with(:name=>'ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$ctl04$mFormattedNumberTextBox').value = type + '/' + maxApplication.to_s + '/' + ENV['MORPH_PERIOD'].to_s
-  button = form.button_with(:value => "Search")
-  list = form.click_button(button)
+daTypes.each do |type|
+  puts "Going to scrape '#{type}' type of applications"
 
-  table = list.search("table.ContentPanel")
-  unless ( table.empty? )
-    error  = 0
-    tr     = table.search("tr.ContentPanel")
+  maxApplication = dbLookup(type)
+  error = 0
+  cont = true
+  while cont do
+    form = page.form
+    form.field_with(:name=>'ctl00$MainBodyContent$mGeneralEnquirySearchControl$mTabControl$ctl04$mFormattedNumberTextBox').value = type + '/' + maxApplication.to_s + '/' + ENV['MORPH_PERIOD'].to_s
+    button = form.button_with(:value => "Search")
+    list = form.click_button(button)
 
-    # fine tuning 'address' field
-    address = tr.search('span')[2].inner_text.strip
-    if address.split(',').size >= 3
-      address = address.split(',', 2)[1].strip
-    end
+    table = list.search("table.ContentPanel")
+    unless ( table.empty? )
+      error  = 0
+      tr     = table.search("tr.ContentPanel")
 
-    record = {
-      'council_reference' => tr.search('a').inner_text,
-      'address'           => address,
-      'description'       => tr.search('span')[1].inner_text.gsub("\n", '. ').squeeze(' '),
-      'info_url'          => base_url + tr.search('a')[0]['href'],
-      'comment_url'       => comment_url,
-      'date_scraped'      => Date.today.to_s,
-      'date_received'     => Date.parse(tr.search('span')[0].inner_text).to_s,
-    }
+      # fine tuning 'address' field, remove 'building name'
+      address = tr.search('span')[2].inner_text.strip
+      if address.split(',').size >= 3
+        address = address.split(',', 2)[1].strip
+      end
 
-    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-      puts "Saving record " + record['council_reference'] + ", " + record['address']
-#       puts record
-      ScraperWiki.save_sqlite(['council_reference'], record)
+      record = {
+        'council_reference' => tr.search('a').inner_text,
+        'address'           => address,
+        'description'       => tr.search('span')[1].inner_text.gsub("\n", '. ').squeeze(' '),
+        'info_url'          => base_url + tr.search('a')[0]['href'],
+        'comment_url'       => comment_url,
+        'date_scraped'      => Date.today.to_s,
+        'date_received'     => Date.parse(tr.search('span')[0].inner_text).to_s,
+      }
+
+      if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+        puts "Saving record " + record['council_reference'] + ", " + record['address']
+#         puts record
+        ScraperWiki.save_sqlite(['council_reference'], record)
+      else
+        puts 'Skipping already saved record ' + record['council_reference']
+      end
     else
-      puts 'Skipping already saved record ' + record['council_reference']
+      error += 1
     end
-  else
-    error += 1
-  end
 
-  # increase i value and scan the next DA
-  maxApplication += 1
-  if error == 10
-    cont = false
+    # increase i value and scan the next DA
+    maxApplication += 1
+    if error == 10
+      cont = false
+    end
   end
 end
