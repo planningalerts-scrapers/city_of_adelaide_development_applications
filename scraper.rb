ac15314ba17efb674683b890fc0a0909a5d907cc
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 23:46:18 +1000

    Interpreting returns as full stops in description doesn't work. Remove this

--- scraper.rb
+++ scraper.rb
@@ -9,12 +9,10 @@ EpathwayScraper.scrape(
   "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd",
   list_type: :all_year, year: year
 ) do |record|
-  # Do some last-minute tweaking of the address and description
   # fine tuning 'address' field, remove 'building name'
   if record["address"].split(',').size >= 3
     record["address"] = record["address"].split(',', 2)[1].strip
   end
-  record["description"]= record["description"].gsub("\n", '. ').squeeze(' ')
 
   EpathwayScraper.save(record)
 end
