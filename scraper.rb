15045ced69bcb72a344af4eeb0aa780c019bbe1a
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 23:29:56 +1000

    Moved code to library

--- scraper.rb
+++ scraper.rb
@@ -14,20 +14,11 @@ page = EpathwayScraper::Page::ListSelect.pick(page, :all)
 
 page = EpathwayScraper::Page::Search.click_date_search_tab(page, scraper.agent)
 
-from = page.form.field_with(name: /FromDatePicker/)
-to = page.form.field_with(name: /ToDatePicker/)
-
-from_date = Date.new(ENV["MORPH_PERIOD"].to_i, 1, 1)
-from.value = from_date.strftime("%d/%m/%Y")
-
-to_date = Date.new(ENV["MORPH_PERIOD"].to_i + 1, 1, 1).prev_day
-# By default the to date is set to today's date. We can't use a later date
-# otherwise the search doesn't work
-if to_date < Date.strptime(to.value, "%d/%m/%Y")
-  to.value = to_date.strftime("%d/%m/%Y")
-end
-
-EpathwayScraper::Page::Search.click_search(page)
+EpathwayScraper::Page::DateSearch.pick_date_range(
+  page,
+  Date.new(ENV["MORPH_PERIOD"].to_i, 1, 1),
+  Date.new(ENV["MORPH_PERIOD"].to_i + 1, 1, 1).prev_day
+)
 
 EpathwayScraper::Page::Index.scrape_all_index_pages(nil, scraper.base_url, scraper.agent) do |record|
   # Do some last-minute tweaking of the address and description
