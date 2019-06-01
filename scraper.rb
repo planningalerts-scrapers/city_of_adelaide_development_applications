63482c978f0168cfa7534462b49e91e069add3ce
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 23:42:53 +1000

    Use more of the library

--- scraper.rb
+++ scraper.rb
@@ -5,23 +5,10 @@ year = ENV["MORPH_PERIOD"].to_i
 
 puts "Getting data in year '#{year}', changable via MORPH_PERIOD environment"
 
-scraper = EpathwayScraper::Scraper.new(
-  "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd"
-)
-
-page = scraper.agent.get(scraper.base_url)
-page = EpathwayScraper::Page::ListSelect.follow_javascript_redirect(page, scraper.agent)
-page = EpathwayScraper::Page::ListSelect.pick(page, :all)
-
-page = EpathwayScraper::Page::Search.click_date_search_tab(page, scraper.agent)
-
-EpathwayScraper::Page::DateSearch.pick_date_range(
-  page,
-  Date.new(year, 1, 1),
-  Date.new(year + 1, 1, 1).prev_day
-)
-
-EpathwayScraper::Page::Index.scrape_all_index_pages(nil, scraper.base_url, scraper.agent) do |record|
+EpathwayScraper.scrape(
+  "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd",
+  list_type: :all_year, year: year
+) do |record|
   # Do some last-minute tweaking of the address and description
   # fine tuning 'address' field, remove 'building name'
   if record["address"].split(',').size >= 3
