d40388da4283b171588717ffbe33d9a2933aca5c
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 11:56:10 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,6 @@
 require "epathway_scraper"
 
-ENV['MORPH_PERIOD'] ||= DateTime.now.year.to_s
-year = ENV["MORPH_PERIOD"].to_i
-
-puts "Getting data in year '#{year}', changable via MORPH_PERIOD environment"
-
 EpathwayScraper.scrape_and_save(
   "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd",
-  list_type: :all_year, year: year
+  list_type: :all_this_year, state: "SA"
 )
