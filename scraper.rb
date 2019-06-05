cae8ca6dde16755a84234688de0a1dbb215ef5d8
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:52:22 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
-  "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd",
-  list_type: :all_this_year, state: "SA"
-)
+EpathwayScraper.scrape_and_save_authority(:adelaide)
