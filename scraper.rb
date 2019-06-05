9aaede6fa07ed75db91b1073e6cc2843ed05151e
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 11:00:21 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -5,14 +5,7 @@ year = ENV["MORPH_PERIOD"].to_i
 
 puts "Getting data in year '#{year}', changable via MORPH_PERIOD environment"
 
-EpathwayScraper.scrape(
+EpathwayScraper.scrape_and_save(
   "https://epathway.adelaidecitycouncil.com/epathway/ePathwayProd",
   list_type: :all_year, year: year
-) do |record|
-  # fine tuning 'address' field, remove 'building name'
-  if record["address"].split(',').size >= 3
-    record["address"] = record["address"].split(',', 2)[1].strip
-  end
-
-  EpathwayScraper.save(record)
-end
+)
